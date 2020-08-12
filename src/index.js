2f50fc6180a48c9c4992a648062f8e3969377693
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-12 19:05:18 +0700

    add Routers to app

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-const db = require("./config/db");
+const route = require("./routes");
 
+const db = require("./config/db");
 // Connect to db
 db.connect();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.send("Hello from Homepage");
-});
+route(app);
 
 app.listen(PORT, () =>
 	console.log(`Server Running on port: http://localhost:${PORT}`)
