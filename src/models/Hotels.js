f1cd40b2ff66f10fb0fc49df37b7166f757929c8
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-12 19:01:21 +0700

    validation data of hotels

---
+++ src/models/Hotels.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const Hotels = new Schema({
+	name: { type: String, maxLength: 255 },
+	price: { type: String, maxLength: 255 },
+	rating: { type: String, maxLength: 255 },
+	imageSource: { type: String, maxLength: 255 },
+	imageSource: { type: String, maxLength: 255 },
+});
+
+module.exports = mongoose.model("hotels", Hotels);
